922850bf1e5e2b74411b466aa07b678ad080652f
SRCinVA <scitarella@hotmail.com>
2021-11-29 21:00:32 -0500

    added escape step

--- tutorial.py
+++ tutorial.py
@@ -10,13 +10,29 @@ def start_screen(stdscr): # we need access to stdscr to write things to the scre
     stdscr.getkey() # in practice, this adds a delay that will stop the screen from automatically closing.
 
 def wpm_test(stdscr):
-    target_test = "Hello world this is a text."
+    target_text = "Hello world this is a text."
     current_text = []
-    stdscr.clear()
-    stdscr.addstr(target_text) # color_pair() is built-in.
+  
     stdscr.refresh()
-    stdscr.getkey()
-    
+
+    while True:
+        key = stdscr.getkey() # this waits for the user to type something
+        
+        if ord(key) == 27: # the ASCII representation of your keyboard for 'esc'
+            break
+
+        current_text.append(key) # when they type, it gets appedned to the current text
+
+        stdscr.clear() # if you don't clear the screen, it will add back everything you've typed so far
+        stdscr.addstr(target_text) # color_pair() is built-in.
+
+        for char in current_text: # next, we loop through every character that they've typed
+            stdscr.addstr(char, curses.color_pair(1)) # we'll display those characters on the screen in a different color.
+
+         stdscr.refresh() # then, we refresh the screen
+
+        
+
 # curses will involve some atypical syntax
 def main(stdscr): # the input gives you a "superimposed screen"
     # using I.D. 1, it's green foreground and a white background.
@@ -26,6 +42,7 @@ def main(stdscr): # the input gives you a "superimposed screen"
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main) # strangely, 'wrapper' is a function that we pass main() to.
                 # it'll call this function while initializing everything to this module.
